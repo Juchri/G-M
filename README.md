6990db02142bd3fe727a211a8487cf44b6939fe4
Juchri <juliette.chri@gmail.com>
2021-05-28 17:46:06 +0200

    form-added

--- README.md
+++ README.md
@@ -7,3 +7,30 @@ Pour compiler Sass :
 ```
 sass --watch sass/main.scss style.css
 ```
+
+
+Firebase
+
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.6.3/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/8.6.3/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyA3seU2bOMm8dkYmuZnbVuICF3jbLpY8Tk",
+    authDomain: "garanceplusmaxime.firebaseapp.com",
+    projectId: "garanceplusmaxime",
+    storageBucket: "garanceplusmaxime.appspot.com",
+    messagingSenderId: "862435938875",
+    appId: "1:862435938875:web:f8bdcfc3e84f1ac8cdc21e",
+    measurementId: "G-49G7R6BDZS"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
---
+++ app.js
@@ -0,0 +1,64 @@
+
+
+//Use to Grab onto Firebase -> Connection
+var firebaseConfig = {
+  apiKey: "AIzaSyA3seU2bOMm8dkYmuZnbVuICF3jbLpY8Tk",
+  authDomain: "garanceplusmaxime.firebaseapp.com",
+  projectId: "garanceplusmaxime",
+  storageBucket: "garanceplusmaxime.appspot.com",
+  messagingSenderId: "862435938875",
+  appId: "1:862435938875:web:f8bdcfc3e84f1ac8cdc21e",
+  measurementId: "G-49G7R6BDZS"
+};
+
+//Init Firebase
+firebase.initializeApp(firebaseConfig);
+console.log(firebase);
+var firestore = firebase.firestore();
+
+//Strat grabbing our DOM element
+const submitBtn = document.querySelector("#submit");
+
+let userName = document.querySelector('#userName');
+let userFirstName = document.querySelector('#userFirstName');
+let userEmail = document.querySelector('#userEmail');
+let userNumber = document.querySelector('#userNumber');
+let userIsComingToCocktail = document.querySelector('#userIsComingToCocktail');
+let userIsComingToDiner = document.querySelector('#userIsComingToDiner');
+let userIsComingToBrunch = document.querySelector('#userIsComingToBrunch');
+let userMessage = document.querySelector('#userMessage');
+
+
+
+const db = firestore.collection("contactData");
+
+submitBtn.addEventListener('click', function (){
+  let userNameInput = userName.value;
+  let userFirstNameInput = userFirstName.value;
+  let userEmailInput = userEmail.value;
+  let userNumberInput = userNumber.value;
+  let userIsComingToCocktailInput = userIsComingToCocktail.checked;
+  let userIsComingToDinerInput = userIsComingToDiner.checked;
+  let userIsComingToBrunchInput = userIsComingToBrunch.checked;
+  let userMessageInput = userMessage.value;
+
+// Access the Database Collection
+
+db.doc()
+  .set({
+    name: userNameInput,
+    firstName: userFirstNameInput,
+    email: userEmailInput,
+    number: userNumberInput,
+    Cocktail: userIsComingToCocktailInput,
+    Diner: userIsComingToDinerInput,
+    Brunch: userIsComingToBrunchInput,
+    message: userMessageInput,
+  })
+  .then(function(){
+    console.log("Data Saved");
+  })
+  .catch (function(error) {
+    console.log(error);
+  });
+});
